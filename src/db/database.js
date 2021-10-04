b838468f200cdba57d5233f32489af1c88208773
Alejandra Bedoya <alejandrabedoya00@gmail.com>
2021-10-03 20:03:32 -0500

    Get teachers

--- src/db/database.js
+++ src/db/database.js
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
 import dbConf from "../config/index"
-require("dotenv").config({ path: "variables.env" });
+require("dotenv").config({ path: ".env" });
 
 
 export default class servicioPG {
   constructor() {
-	const DATAENV = dbConf[process.env.NODE_ENV] || 'development'
+	const DATAENV = dbConf[process.env.NODE_ENV] || dbConf['development']
+    console.log(DATAENV)
     this.pool = new Pool(DATAENV);
   }
 
