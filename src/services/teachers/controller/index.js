1ef7c3a8a8fd46d0ec6905fcfe8c444d123b3d53
Alejandra Bedoya <alejandrabedoya00@gmail.com>
2021-10-03 20:30:47 -0500

    findTeachers

--- src/services/teachers/controller/index.js
+++ src/services/teachers/controller/index.js
@@ -3,13 +3,12 @@ import TeacherDao from "../Dao";
 export default class TeacherController {
   constructor() {
     this.dao = new TeacherDao();
-    //this.daoStudents = new StudentsDao();
   }
 
   async findTeachers() {
     try {
-      const data = await this.dao.findTeachers( );
-      return { message: "melo", data };
+      const data = await this.dao.findTeachers();
+      return { message: "Profesores consultados", data: data.rows };
     } catch (error) {
       throw new Error("error while executing findTeachers " + error.message);
     }
